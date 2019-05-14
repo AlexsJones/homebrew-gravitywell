d1b0bddd795d0a26eb24065124148af14e362b50
Alex Jones <AlexsJones@users.noreply.github.com>
2019-05-14 12:23:40 +0100

    Update gravitywell.rb

--- gravitywell.rb
+++ gravitywell.rb
@@ -1,8 +1,8 @@
 class Gravitywell < Formula
   desc "Turn a pool of Docker hosts into a single, virtual host"
   homepage "https://github.com/AlexsJones/gravitywell"
-  url "https://github.com/AlexsJones/gravitywell/releases/download/v0.3.2/gravitywell_0.3.2_Darwin_amd64.tar.gz"
-  sha256 "4b0d566830d32e8ca47d36e25dbf2e0de265e7aad822cb2ff679574ee9e475b5"
+  url "https://github.com/AlexsJones/gravitywell/releases/download/v0.3.5/gravitywell_0.3.5_Darwin_amd64.tar.gz"
+  sha256 "79722737208d3f4694229079987fff2b24e3ea16dea43c90872e7f59986fcb6f"
   head "https://github.com/AlexsJones/gravitywell.git"
 
   def install
